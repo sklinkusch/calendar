38df8036f743f28735452efc3dd319657eaaf557
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-03-09 15:20:24 +0100

    general card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import styles from './Card.module.css';
+
+type Cdate = {
+  day?: number;
+  month?: string;
+  year?: number;
+  time?: string;
+  weekday?: string;
+};
+
+type CardProps = {
+  date: Cdate;
+};
+
+export function Card({ date }: CardProps) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.monthyear}>
+        <span>{date.month}</span> <span>{date.year}</span>
+      </div>
+      <div className={styles.day}>{date.day}</div>
+      <div className={styles.weekday}>
+        <span>{date.weekday}</span>
+      </div>
+      <div>{date.time && <span>{date.time}</span>}</div>
+    </div>
+  );
+}
