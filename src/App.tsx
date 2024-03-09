adc324449b83756d5b8ac29fc01964033d97d2a9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-03-09 17:01:53 +0100

    add document.title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { IslamicCalendar } from './components/IslamicCalendar/IslamicCalendar';
 import { GregorianCalendar } from './components/GregorianCalendar/GregorianCalendar';
+import { useEffect } from 'react';
 import styles from './App.module.css';
 
 function App() {
+  useEffect(() => {
+    window.document.title = 'Calendars';
+  }, []);
   return (
     <div className={styles.grid}>
       <GregorianCalendar />
