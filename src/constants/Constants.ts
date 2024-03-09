15e8e37ba61a0702f929ff280629c2da9586eb10
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-03-09 15:20:09 +0100

    new constants file

---
+++ src/constants/Constants.ts
@@ -0,0 +1,32 @@
+export const daysPerMonth: { [key: string]: number } = {
+  '0': 31,
+  '1': 28,
+  '1l': 29,
+  '2': 31,
+  '3': 30,
+  '4': 31,
+  '5': 30,
+  '6': 31,
+  '7': 31,
+  '8': 30,
+  '9': 31,
+  '10': 30,
+  '11': 31,
+};
+
+export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
